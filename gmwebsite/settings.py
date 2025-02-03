9e89c06dbf6a21aed32afdbe76c738b40730f78c
swati gaikwad <swatirgaikwad1436@gmail.com>
2025-02-03 16:15:09 +0530

    Initial commit

--- gmwebsite/settings.py
+++ gmwebsite/settings.py
@@ -89,12 +89,23 @@ WSGI_APPLICATION = 'gmwebsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',  # Ensure 'BASE_DIR' is correctly defined
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Ensure 'BASE_DIR' is correctly defined
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myuser',  # Aapne jo database naam banaya
+        'USER': 'postgres',  # PostgreSQL user
+        'PASSWORD': 'swati',  # PostgreSQL password
+        'HOST': 'localhost',  # Agar local PostgreSQL use kar rahe ho
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
@@ -157,7 +168,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Set DEBUG to True only in development
-DEBUG = False  # Set to True in development
+DEBUG = True  # Set to True in development
 
 # Hostnames allowed to serve your Django app (add your domain and localhost)
 ALLOWED_HOSTS = [
