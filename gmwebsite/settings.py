dc1ebb4de5fc52d2be1c8a1eb12938c7295c3ece
swati gaikwad <swatirgaikwad1436@gmail.com>
2025-02-04 17:29:07 +0530

    Updated requirements.txt

--- gmwebsite/settings.py
+++ gmwebsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 # import os
 # from dotenv import load_dotenv
 
@@ -109,12 +109,12 @@ WSGI_APPLICATION = 'gmwebsite.wsgi.application'
 # DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://postgres:swati@localhost:5432/myuser')
 
 # DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://swati:swati@ec2-54-123-45-67.compute-1.amazonaws.com:5432/myuser')
-
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://swati:swati@ec2-54-123-45-67.compute-1.amazonaws.com:5432/myuser')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-    
-
       
 
     # 'default': {
